1c7158b968bbe8cfef726cbd907a8d7e4b15b09a
NelsonKioko <kiokonelson2@gmail.com>
2019-05-13 16:34:40 +0300

    create models,admin and register models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . import models
+admin.site.register(models.Image)
+admin.site.register(models.Location)
 
-# Register your models here.
--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+   image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+   image_name = models.CharField(max_length=100)
+   image_description = models.TextField()
+
+   def __str__(self):
+       return self.image_name
+
+
+class Location(models.Model):
+   image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
