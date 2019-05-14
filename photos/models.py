8b7227cce45f2228802d673dcb67b4aa0b329d3d
NelsonKioko <kiokonelson2@gmail.com>
2019-05-14 14:50:44 +0300

    create cards

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Image(models.Model):
-   image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+   image = models.ImageField(default='default.jpeg', upload_to='pictures')
    image_name = models.CharField(max_length=100)
    image_description = models.TextField()
 
